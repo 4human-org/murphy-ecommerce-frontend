051f45033f0896145af8ce465f33bf9bf2065b76
Ruslan M <r1554899@gmail.com>
2024-01-27 18:18:34 -0800

    hydration fix!

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-// import Navbar from "../components/navbar";
-import CategoryDisplaySection from "@/components/CategoryDisplaySection";
+import Navbar from "../components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,9 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <CategoryDisplaySection />
-      <CategoryDisplaySection />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <Navbar />
+      {children}
+      </body>
     </html>
   );
 }
