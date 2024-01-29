fc8ad7f01be3fb4f373b949515ea6c15c96db939
Liam DeWitt <mugzy550@gmail.com>
2024-01-28 21:01:36 -0500

    add dummy data file

---
+++ components/DummyData.js
@@ -0,0 +1,64 @@
+export default function getDummyData() {
+  return [
+    {
+      productName: "Product 1",
+      price: 23.5,
+      imageURL:
+        "https://dcist.com/wp-content/uploads/sites/3/2023/10/20230829_baby-monkey_Fenston_6-1024x1022.jpg",
+    },
+    {
+      productName: "Product 2",
+      price: 3.25,
+      imageURL:
+        "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg",
+    },
+    {
+      productName: "Product 3",
+      price: 82.0,
+      imageURL:
+        "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*",
+    },
+    {
+      productName: "Product 4",
+      price: 10.5,
+      imageURL:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Eopsaltria_australis_-_Mogo_Campground.jpg/640px-Eopsaltria_australis_-_Mogo_Campground.jpg",
+    },
+    {
+      productName: "Product 5",
+      price: 7.99,
+      imageURL:
+        "https://animals.sandiegozoo.org/sites/default/files/2016-11/animals_hero_lizards.jpg",
+    },
+    {
+      productName: "Product 6",
+      price: 999.99,
+      imageURL:
+        "https://cdn.vox-cdn.com/thumbor/WoxFzJA32z22UpHq0OZKe6QXt6E=/1400x788/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/25166446/Godzilla_x_Kong_pinkzilla.png",
+    },
+    {
+      productName: "Product 7",
+      price: 100.5,
+      imageURL:
+        "https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2Fhlbj9ceadoh91.jpg",
+    },
+    {
+      productName: "Product 8",
+      price: 101.5,
+      imageURL:
+        "https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2Fhlbj9ceadoh91.jpg",
+    },
+    {
+      productName: "Product 9",
+      price: 102.5,
+      imageURL:
+        "https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2Fhlbj9ceadoh91.jpg",
+    },
+    {
+      productName: "Product 10",
+      price: 103.5,
+      imageURL:
+        "https://www.reddit.com/media?url=https%3A%2F%2Fi.redd.it%2Fhlbj9ceadoh91.jpg",
+    },
+  ];
+}
