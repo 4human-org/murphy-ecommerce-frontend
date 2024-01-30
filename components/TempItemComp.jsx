5d7b6f351a41c18327bf0f7e0f2062e4074160e4
Liam DeWitt <mugzy550@gmail.com>
2024-01-30 17:01:00 -0500

    add bold text to price add clause for when there are no image links or image key

--- components/TempItemComp.jsx
+++ components/TempItemComp.jsx
@@ -1,15 +1,27 @@
 "use client";
 
+import limitString from "../utils/LimitString.js";
+
 export default function TempItemComp({ productName, price, imageURL }) {
-  // remove grid row gap and add y padding - adjust max-h-28
+  const title = limitString(productName, 9);
+  const cost = price?.toFixed(2);
+  const img = imageURL ? (
+    <img
+      src={imageURL}
+      className="w-full h-full max-h-28 max-w-56 rounded-xl object-cover"
+    />
+  ) : (
+    <img
+      src="https://img.freepik.com/premium-vector/default-image-icon-vector-missing-picture-page-website-design-mobile-app-no-photo-available_87543-11093.jpg"
+      className="w-full h-full max-h-28 max-w-56 rounded-xl object-fit"
+    />
+  );
+
   return (
-    <div className="text-lg">
-      <img
-        src={imageURL}
-        className="w-full h-full max-h-28 max-w-56 rounded-xl object-cover"
-      />
-      <h4 className="mt-2">{productName}</h4>
-      <h4>${price?.toFixed(2)}</h4>
+    <div className="p-4 text-xs transition ease-in-out duration-300 rounded-xl hover:bg-murphy-hover sm:text-lg">
+      {img}
+      <h4 className="mt-2">{title}</h4>
+      <h4 className="font-semibold">${cost}</h4>
     </div>
   );
 }
