f0e837d65a3e56aa07539d8fe2e4570eb184d3ed
Liam DeWitt <mugzy550@gmail.com>
2024-01-30 16:58:52 -0500

    add custom gray color to match figma and responsive classes

--- components/CategorySection.jsx
+++ components/CategorySection.jsx
@@ -3,11 +3,12 @@
 import GridItemComp from "./GridItemComp.jsx";
 
 export default function CategorySection({ sectionHeader }) {
-  //
   return (
     <section className="flex flex-col justify-container items-center mb-24">
-      <div className="px-8 bg-red-500 mx-2 rounded-3xl md:mx-6 xl:mx-16">
-        <h1 className="py-2 text-2xl py-5">{sectionHeader}</h1>
+      <div className="px-8 bg-murphy-gray mx-2 rounded-3xl md:mx-6 xl:mx-16">
+        <h1 className="py-2 text-2xl py-5 leading-relaxed tracking-wide">
+          {sectionHeader}
+        </h1>
         <GridItemComp />
       </div>
     </section>
