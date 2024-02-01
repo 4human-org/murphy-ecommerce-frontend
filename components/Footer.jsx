369cafe307e34f45f88c97c3546d8a6b2fdce90b
Connor Skudlarek <connor.skudlarek@gmail.com>
2024-01-31 17:56:37 -0800

    Change footer links to needed values

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,28 +1,31 @@
-"use client"
+"use client";
 import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className="text-center flex flex-col bg-neutral-100/75 h-fill bottom-0 w-screen">
-      <nav className="flex flex-col p-4 space-y-4 ">
-        <ul className="flex flex-row p-4 space-x-10 place-content-center">
+      <nav className="md:flex md:flex-col md:p-4 md:space-y-4">
+        <ul className="p-6 md:flex md:flex-row md:p-4 md:space-x-10 md:place-content-center">
           <Link href="/#">
-            <li id="about"></li>About
+            <li id="about">About</li>
           </Link>
           <Link href="/#">
-            <li id="blog">Blog</li>
+            <li id="contact">Contact</li>
           </Link>
           <Link href="/#">
-            <li id="jobs">Jobs</li>
-          </Link>
-          <Link href="/#">
-            <li id="press">Press</li>
+            <li id="faq">FAQs</li>
           </Link>
           <Link href="/#">
             <li id="accessibility">Accessibility</li>
           </Link>
           <Link href="/#">
             <li id="partners">Partners</li>
+          </Link>
+          <Link href="/#">
+            <li id="terms-of-use">Terms of Use</li>
+          </Link>
+          <Link href="/#">
+            <li id="privacy-policy">Privacy Policy</li>
           </Link>
         </ul>
         <ul className="flex flex-row p-4 space-x-10 place-content-center">
@@ -36,14 +39,14 @@ export default function Footer() {
             <li id="twitter">X</li>
           </Link>
           <Link href="/#">
-            <li id="github">G</li>
-          </Link>
-          <Link href="/#">
             <li id="youtube">Y</li>
           </Link>
         </ul>
       </nav>
-      <small id="copyright" className="p-4">Copyright © {(() => new Date().getFullYear())()} Murphy Charitable Foundation Uganda, 501{"(c)"}3. All rights reserved.</small>
+      <small id="copyright" className="p-4">
+        Copyright © {(() => new Date().getFullYear())()} Murphy Charitable
+        Foundation Uganda. All rights reserved.
+      </small>
     </footer>
   );
 }
