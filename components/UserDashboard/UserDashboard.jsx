66e8c0886465217d012cb277dfbef0ba9f39c3a3
Nia Bennabhaktula <niabenna@gmail.com>
2024-02-22 22:20:36 -0700

    Added recentDonations.jsx

---
+++ components/UserDashboard/UserDashboard.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import RecentDonations from "./RecentDonations";
+import UserProfile from "../UserProfile/UserProfileComp.jsx";
+
+export default function UserDashboard() {
+
+      return (
+        <RecentDonations/>
+      );
+}
