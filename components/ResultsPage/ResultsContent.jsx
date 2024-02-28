bc171e85b4e43be5f40a3e6df2d91309f6d2e117
Connor Skudlarek <connor.skudlarek@gmail.com>
2024-02-28 11:58:31 -0800

    Move Suspense import into same line

--- components/ResultsPage/ResultsContent.jsx
+++ components/ResultsPage/ResultsContent.jsx
@@ -1,39 +1,39 @@
-"use client"
+"use client";
 
 import Products from "./Products";
 import { useSearchParams } from "next/navigation";
-import { useState, useEffect } from "react";
-import { Suspense } from "react";
+import { useState, useEffect, Suspense } from "react";
 
 export default function ResultsContent() {
-    const searchParams = useSearchParams();
-    const [query, setQuery] = useState("");
-    // Sets current page to inital searchParams, default value is 1
-    const [currentPage, setCurrentPage] = useState(
-      Number(searchParams.get("page"))
-    );
-  
-    // UseEffect to handle inital state of query based on searchParams
-    useEffect(() => {
-      // If query is exists in searchParams, update query state accordingly
-      if (searchParams.get("query")) {
-        setQuery(searchParams.get("query").toString());
-  
-        // If query gets deleted by user and gets set to null, update query to empty string
-      } else if (searchParams.get("query")?.toString() == null) {
-        setQuery("");
-      }
-    }, [searchParams]);
-  
-    return (
-      <div>
-        <div className="flex items-center justify-between ">
-          <Suspense>{query ? <h1 className="text-2xl">Results for {query}</h1> : null}</Suspense>
-        </div>
+  const searchParams = useSearchParams();
+  const [query, setQuery] = useState("");
+  // Sets current page to inital searchParams, default value is 1
+  const [currentPage, setCurrentPage] = useState(
+    Number(searchParams.get("page"))
+  );
+
+  // UseEffect to handle inital state of query based on searchParams
+  useEffect(() => {
+    // If query is exists in searchParams, update query state accordingly
+    if (searchParams.get("query")) {
+      setQuery(searchParams.get("query").toString());
+
+      // If query gets deleted by user and gets set to null, update query to empty string
+    } else if (searchParams.get("query")?.toString() == null) {
+      setQuery("");
+    }
+  }, [searchParams]);
+
+  return (
+    <div>
+      <div className="flex items-center justify-between ">
         <Suspense>
-          <Products query={query} currentPage={currentPage} />
+          {query ? <h1 className="text-2xl">Results for {query}</h1> : null}
         </Suspense>
       </div>
-    );
-  }
-  
+      <Suspense>
+        <Products query={query} currentPage={currentPage} />
+      </Suspense>
+    </div>
+  );
+}
