598f2d2bbc1e826bdebe43c2bc470c51b0325e80
Connor Skudlarek <connor.skudlarek@gmail.com>
2024-02-28 11:55:34 -0800

    Move cart page to own page, corrects minor routing, naming, and CSS issues

---
+++ app/cart/page.jsx
@@ -0,0 +1,9 @@
+import CartPage from "../../components/CartPage/CartPage";
+
+export default function Page() {
+  return (
+    <main className="flex-col flex w-screen overflow-x-hidden scroll-smooth mt-12">
+      <CartPage />
+    </main>
+  );
+}
