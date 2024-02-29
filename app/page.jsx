136733895c1f8fbda14e3c94ace7a43b83b36ac6
Connor Skudlarek <connor.skudlarek@gmail.com>
2024-02-28 19:31:29 -0800

    Add use client to the main app page

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import Search from '@/components/Search';
 import CategorySection from '../components/CategorySection/CategorySection';
 import { Suspense } from 'react';
@@ -5,11 +6,11 @@ import { Suspense } from 'react';
 export default function Page() {
   return (
     <main className='flex-col flex w-screen overflow-x-hidden scroll-smooth mt-12'>
-      <Suspense>
-        <Search placeholder='Search products...' />
-      </Suspense>
-      <CategorySection sectionHeader='Recommended Items' />
-      <CategorySection sectionHeader='Popular Items' />
+        <Suspense>
+          <Search placeholder='Search products...' />
+        </Suspense>
+        <CategorySection sectionHeader='Recommended Items' />
+        <CategorySection sectionHeader='Popular Items' />
     </main>
   );
 }
