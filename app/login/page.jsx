391cf9d69005ba94f95440c0a4fa294a21e057e5
Connor Skudlarek <connor.skudlarek@gmail.com>
2024-03-13 12:55:08 -0700

    Add Suspense to fix error at useSearchParams()

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,11 @@
+"use client";
+import { Suspense } from "react";
 import LoginPage from "../../components/LoginPage/LoginPage";
 
 export default function Page() {
-  return <LoginPage />;
+  return (
+    <Suspense>
+      <LoginPage />
+    </Suspense>
+  );
 }
