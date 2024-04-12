377a4217142ce55c690c555baa77b75fff76d9f6
Eric Lin <el3136@nyu.edu>
2024-04-11 23:14:19 -0400

    Add correct social media links and icons

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,9 +2,12 @@
 import Link from "next/link";
 // next/link signifies to Next.JS to pre-render the page it links to
 // which helps speed up loading times for those pages
+import { Instagram, Facebook, Linkedin, Music2, Twitter, Youtube } from 'lucide-react';
 
 // footer , nav, and small elements for semantic HTML and accessibility
 export default function Footer() {
+  const imgUrl = "/logo.webp";
+
   return (
     <footer className="text-center flex flex-col bg-neutral-100/75 h-fill bottom-0 w-screen">
       <nav className="md:flex md:flex-col md:p-4 md:space-y-4">
@@ -32,17 +35,29 @@ export default function Footer() {
           </Link>
         </ul>
         <ul className="flex flex-row p-4 space-x-10 place-content-center">
-          <Link href="/#">
-            <li id="facebook">F</li>
+          <Link href="http://www.murphycharity.org/">
+            <li id="website"> <img className="rounded-full h-6" src={imgUrl} /> </li>
           </Link>
-          <Link href="/#">
-            <li id="instagram">I</li>
+          <Link href="https://instagram.com/murphycharity_?igshid=MTI1ZDU5ODQ3Yw==">
+            <li id="instagram"> <Instagram /> </li>
           </Link>
-          <Link href="/#">
-            <li id="twitter">X</li>
+          <Link href="https://www.facebook.com/murphycharityuganda/">
+            <li id="facebook"> <Facebook /> </li>
           </Link>
-          <Link href="/#">
-            <li id="youtube">Y</li>
+          <Link href="https://www.linkedin.com/in/murphy-charitable-foundation-uganda-70948b255/">
+            <li id="linkedin_profile"> LinkedIn </li>
+          </Link>
+          <Link href="https://www.linkedin.com/company/murphy-charitable-foundation-uganda/">
+            <li id="linkedin_page"> <Linkedin /> </li>
+          </Link>
+          <Link href="https://www.tiktok.com/@murphycharity_?_t=8dUTaiyws1a&_r=1">
+            <li id="tiktok"> <Music2 /> </li>
+          </Link>
+          <Link href="https://twitter.com/murphycharity">
+            <li id="twitter"> <Twitter /> </li>
+          </Link>
+          <Link href="https://www.youtube.com/@murphycharity">
+            <li id="youtube"> <Youtube /> </li>
           </Link>
         </ul>
       </nav>
