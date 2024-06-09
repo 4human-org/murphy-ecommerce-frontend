7e985930cf0d8261e14bcc6c2cbd0fd9a774bc06
Connor Skudlarek <connor.skudlarek@gmail.com>
2024-06-08 17:35:02 -0700

    Add open new tab to remaining links in footer

--- components/LandingPage/Footer.jsx
+++ components/LandingPage/Footer.jsx
@@ -19,57 +19,72 @@ export default function Footer() {
     <footer className="h-fill bottom-0 flex w-screen flex-col bg-neutral-100/75 text-center">
       <nav className="md:flex md:flex-col md:space-y-4 md:p-4">
         <ul className="p-6 md:flex md:flex-row md:place-content-center md:space-x-10 md:p-4">
-          <Link href="/#">
+          <Link href="/#" target="_blank">
             <li id="about">About</li>
           </Link>
-          <Link href="/#">
+          <Link href="/#" target="_blank">
             <li id="contact">Contact</li>
           </Link>
-          <Link href="/#">
+          <Link href="/#" target="_blank">
             <li id="faq">FAQs</li>
           </Link>
-          <Link href="/#">
+          <Link href="/#" target="_blank">
             <li id="accessibility">Accessibility</li>
           </Link>
-          <Link href="/#">
+          <Link href="/#" target="_blank">
             <li id="partners">Partners</li>
           </Link>
-          <Link href="/#">
+          <Link href="/#" target="_blank">
             <li id="terms-of-use">Terms of Use</li>
           </Link>
-          <Link href="/#">
+          <Link href="/#" target="_blank">
             <li id="privacy-policy">Privacy Policy</li>
           </Link>
         </ul>
         <ul className="flex flex-row place-content-center space-x-10 p-4">
           <Link href="http://www.murphycharity.org/" target="_blank">
-            <li id="website" >
+            <li id="website">
               {" "}
               <img className="h-6 rounded-full" src={imgUrl} />{" "}
             </li>
           </Link>
-          <Link href="https://instagram.com/murphycharity_?igshid=MTI1ZDU5ODQ3Yw==" target="_blank">
+          <Link
+            href="https://instagram.com/murphycharity_?igshid=MTI1ZDU5ODQ3Yw=="
+            target="_blank"
+          >
             <li id="instagram">
               {" "}
               <Instagram />{" "}
             </li>
           </Link>
-          <Link href="https://www.facebook.com/murphycharityuganda/" target="_blank">
+          <Link
+            href="https://www.facebook.com/murphycharityuganda/"
+            target="_blank"
+          >
             <li id="facebook">
               {" "}
               <Facebook />{" "}
             </li>
           </Link>
-          <Link href="https://www.linkedin.com/in/murphy-charitable-foundation-uganda-70948b255/" target="_blank">
+          <Link
+            href="https://www.linkedin.com/in/murphy-charitable-foundation-uganda-70948b255/"
+            target="_blank"
+          >
             <li id="linkedin_profile"> LinkedIn </li>
           </Link>
-          <Link href="https://www.linkedin.com/company/murphy-charitable-foundation-uganda/" target="_blank">
+          <Link
+            href="https://www.linkedin.com/company/murphy-charitable-foundation-uganda/"
+            target="_blank"
+          >
             <li id="linkedin_page">
               {" "}
               <Linkedin />{" "}
             </li>
           </Link>
-          <Link href="https://www.tiktok.com/@murphycharity_?_t=8dUTaiyws1a&_r=1" target="_blank">
+          <Link
+            href="https://www.tiktok.com/@murphycharity_?_t=8dUTaiyws1a&_r=1"
+            target="_blank"
+          >
             <li id="tiktok">
               {" "}
               <Music2 />{" "}
