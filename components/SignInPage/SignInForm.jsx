8fdb0306cfed7667b9683ba9a14f4257ef764391
Kevin Aguirre <kevinaguirre463@gmail.com>
2024-06-19 18:08:10 -0400

    added sign up page, check authenication, need to check email duplicates

--- components/SignInPage/SignInForm.jsx
+++ components/SignInPage/SignInForm.jsx
@@ -4,7 +4,39 @@ export default function SignInForm({ handleSubmit }) {
   return (
     <div className="mx-5 mt-5 flex flex-col">
       <form onSubmit={handleSubmit}>
-        <p className="mb-10 text-xl tracking-wide">Sign In</p>
+        <p className="mb-10 text-xl tracking-wide">Create An Account</p>
+        <div className="relative mt-6">
+          <input
+            type="text"
+            name="firstName"
+            id="firstName"
+            placeholder="First Name"
+            className="peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
+            autoComplete="firstName"
+          />
+          <label
+            htmlFor="firstName"
+            className="pointer-events-none absolute left-0 top-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800"
+          >
+            First Name
+          </label>
+        </div>
+        <div className="relative mt-6">
+          <input
+            type="text"
+            name="lastName"
+            id="lastName"
+            placeholder="Last Name"
+            className="peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
+            autoComplete="Last Name"
+          />
+          <label
+            htmlFor="lastName"
+            className="pointer-events-none absolute left-0 top-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800"
+          >
+            Last Name
+          </label>
+        </div>
         <div className="relative mt-6">
           <input
             type="email"
@@ -38,39 +70,43 @@ export default function SignInForm({ handleSubmit }) {
             Password
           </label>
         </div>
-        <div className="my-12 text-sm font-semibold text-gray-700 underline hover:text-black">
-          <a href="/#">Forgot password?</a>
-        </div>
-        <div className="flex items-center justify-between">
-          <label htmlFor="remember_me" className="block text-sm text-gray-900">
-            Remember me
-          </label>
+        <div className="relative mt-6">
           <input
-            id="rememberMe"
-            name="rememberMe"
-            type="checkbox"
-            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+            type="password"
+            name="confirmPassword"
+            id="confirmPassword"
+            required
+            autoComplete="current-password"
+            placeholder="Confirm Password"
+            className="peer peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
           />
+          <label
+            htmlFor="confirmPassword"
+            className="pointer-events-none absolute left-0 top-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800"
+          >
+            Confirm Password
+          </label>
         </div>
         <div className="my-12 flex justify-center">
           <button
             type="submit"
             className="w-11/12 rounded-full bg-blue-800 p-1.5 text-white hover:bg-blue-900"
           >
-            Sign in
-          </button>
-        </div>
-        <div className="flex text-center text-sm text-gray-500">
-          <p>Don't have an account yet?</p>
-          <a
-            href="/signup"
-            className="ml-1 font-semibold text-gray-600 hover:underline focus:text-gray-800 focus:outline-none"
-          >
             Create Account
-          </a>
-          .
+          </button>
         </div>
       </form>
     </div>
   );
 }
+
+
+/*
+
+
+firstName string 
+lastName string
+isAdmin boolean
+email string 
+
+*/
--- components/SignInPage/SignInPage.jsx
+++ components/SignInPage/SignInPage.jsx
@@ -17,16 +17,50 @@ export default function LoginPage() {
   async function handleSubmit(event) {
     event.preventDefault();
     const form = event.currentTarget;
+    const firstName = form.firstName.value 
+    const lastName = form.lastName.value 
     const email = form.email.value;
     const password = form.password.value;
-    const rememberMe = form.rememberMe.checked;
+    const confirmPassword = form.confirmPassword.value;
+    const isAdmin = false;
 
-    try {
-      await signIn(email, password, rememberMe);
-      router.replace(continueTo);
-    } catch (error) {
-      window.alert(error);
+    if (password !== confirmPassword) {
+      alert('passwords do not match')
+      return;
     }
+
+    // console.log(firstName, lastName, email, password, confirmPassword)
+
+    const data = {firstName, lastName, email, isAdmin}
+
+    fetch('http://localhost:3030/users', {
+      method: 'POST', 
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }).then(response => {
+      if (response.ok) {
+        alert("Successfully created account.")
+        router.push('/login')
+        return response.json();
+      } else {
+        throw new Error('Network response was not ok');
+      }
+    })
+      .then(data => {
+          console.log('Success:', data);
+      })
+      .catch((error) => {
+          console.error('Error:', error);
+      });
+
+    // try {
+    //   await signIn(email, password, rememberMe);
+    //   router.replace(continueTo);
+    // } catch (error) {
+    //   window.alert(error);
+    // }
   }
 
   if (user) {
@@ -38,8 +72,8 @@ export default function LoginPage() {
     <main className="flex">
       <section className="flex flex-row px-16 py-12">
         <SignInForm handleSubmit={handleSubmit} />
-        <SignInSeparator />
-        <AlternativeSignups />
+        {/* <SignInSeparator />
+        <AlternativeSignups /> */}
       </section>
     </main>
   );
