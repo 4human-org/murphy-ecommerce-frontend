f910a9ec4d6bfbd38d8b52560c4827f10ecbe62b
Connor Skudlarek <connor.skudlarek@gmail.com>
2024-06-22 18:47:52 -0700

    Add generic terms of use
    Murphy's site is 404 at terms of use, so nothing to draw from.

---
+++ app/terms-of-use/page.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+export default function Page() {
+  return (
+    <div className="container">
+      <h2 className="text-center text-[60px] font-bold">Terms of Use</h2>
+
+      <ol className="list-number p-6 text-xl">
+        <li className="p-4">
+          1. <strong>Acceptance of Terms</strong>
+          <br></br>By accessing and using the Murphy Foundation Uganda website,
+          you accept and agree to be bound by the terms and provision of this
+          agreement.
+        </li>
+
+        <li className="p-4">
+          2. <strong>Use of Site</strong>
+          <br></br>You may use our site for lawful purposes only and may not use
+          our site to engage in any conduct that restricts or inhibits anyone's
+          use or enjoyment of the site.
+        </li>
+
+        <li className="p-4">
+          3. <strong>Intellectual Property Rights</strong>
+          <br></br>All content, features, and functionality on the site are
+          owned by the Murphy Foundation Uganda and are protected by
+          international copyright laws.
+        </li>
+
+        <li className="p-4">
+          4. <strong>Limitation of Liability</strong>
+          <br></br>Under no circumstance shall we be liable for any direct,
+          indirect, consequential, or any other damages arising out of your use
+          of the site.
+        </li>
+
+        <li className="p-4">
+          5. <strong>Governing Law</strong>
+          <br></br>These terms shall be governed by and construed in accordance
+          with the laws of Uganda.
+        </li>
+      </ol>
+    </div>
+  );
+}
